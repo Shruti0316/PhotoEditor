fb9db3fe24e22e0cfd8f25d0172bf83e3699bd5b
Shruti0316 <shruti16.k@gmail.com>
2021-08-21 17:14:26 +0530

    Added border, rotation

--- script.js
+++ script.js
@@ -9,17 +9,41 @@ var img = document.getElementById("image");
         range.addEventListener("mousemove",function(e){
             img.style.filter = prop +"(" + range.value + "%)";
         })
-        if(prop == "resize"){
-            document.getElementById("size").style.display = "block";
+        // if(prop == "resize"){
+        //     document.getElementById("size").style.display = "block";
+        // }
+        // if(prop == "done"){
+        //     var formEl = document.forms.size;
+        //     var formData = new FormData(formEl);
+        //     var h = formData.get('height')+"px";
+        //     var w = formData.get('width')+"px";
+        //     console.log(w,h);
+        //     document.getElementById("image").width = w ;
+        //     document.getElementById("image").height = h ;
+        //     document.getElementById("size").style.display = "none";
+        // }
+        if(prop == "Aborder"){
+            document.getElementById("border").style.display = "block";
         }
-        if(prop == "done"){
-            var formEl = document.forms.size;
+        if(prop == "bdone"){
+            var formEl = document.forms.border;
             var formData = new FormData(formEl);
-            var h = formData.get('height')+"px";
             var w = formData.get('width')+"px";
-            console.log(w,h);
-            document.getElementById("image").width = w ;
-            document.getElementById("image").height = h ;
-            document.getElementById("size").style.display = "none";
+            var shape = formData.get('shape');
+            var colour = formData.get('colour');
+            document.getElementById("image").style.borderWidth = w;
+            document.getElementById("image").style.borderStyle = shape;
+            document.getElementById("image").style.borderColor = colour;
+            document.getElementById("border").style.display = "none";
+        }
+        if(prop == "rotator"){
+            document.getElementById("rotate").style.display = "block";
+        }
+        if(prop == "rdone"){
+            var formEl = document.forms.rotate;
+            var formData = new FormData(formEl);
+            var angle = formData.get('angle')+"deg";
+            document.getElementById("image").style.transform = "rotate("+angle+")";
+            document.getElementById("border").style.display = "none";
         }
     })
